8a8c66cac3e1d8084d35e17260f83bd2a8c7b460
Francileudo Oliveira <sfrancileudo1@gmail.com>
2023-12-06 14:48:01 -0300

    feat: database table links

--- app/configure_database.py
+++ app/configure_database.py
@@ -1,9 +1,8 @@
 import os
 from database.variables_database import ConfigVariables
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy import Integer, String, Column, JSON
+from sqlalchemy.orm import DeclarativeBase, relationship
+from sqlalchemy import Integer, String, Column, JSON, ForeignKey
 from instance_app import app
 
 
@@ -28,7 +27,11 @@ class User(db.Model):
     naan = Column(String(50))
     organization = Column(db.String(50))
     email = Column(String(120), unique=True, nullable=False)
-    wallet_private_key = Column(String(256))
+    wallet_private_key = Column(String, ForeignKey('wallet.private_key'))
+
+    transactions = relationship("Transaction", back_populates="user_account")
+
+    wallet = relationship("Wallet", back_populates="user_account")
 
 
 class Transaction(db.Model):
@@ -40,12 +43,28 @@ class Transaction(db.Model):
     status = Column(String(50))
     parent_tx = Column(String(255))
 
+    user_account_id = Column(Integer, ForeignKey('user_account.id'))
+    user_account = relationship("UserAccount", back_populates="transactions")
+
 
 class Wallet(db.Model):
     __tablename__ = 'wallet'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     private_key = Column(String(255))
+
+    noid_provider_id = Column(String, ForeignKey('noid_provider.id'))
+    noid_provider = relationship("NoidProvider", back_populates="wallet")
+
+    section_mapping_authority_id = Column(
+        String, ForeignKey('section_mapping_authority.id'))
+    section_mapping_authority = relationship(
+        "SectionMappingAuthority", back_populates="wallet_responsable")
+
+    decentralized_name_mapping_authority_id = Column(
+        String, ForeignKey('decentralized_name_mapping_authority.id'))
+    decentralized_name_mapping_authority = relationship(
+        "DecentralizedNameMappingAuthority", back_populates="wallet_responsable")
 
 
 class NoidProvider(db.Model):
@@ -58,6 +77,14 @@ class NoidProvider(db.Model):
     sep_token = Column(String(1))
     noid_len = Column(Integer)
 
+    decentralized_name_mapping_authority_id = Column(Integer, ForeignKey(
+        'decentralized_name_mapping_authority.id'))
+    decentralized_name_mapping_authority = relationship(
+        "DecentralizedNameMappingAuthority", back_populates="noid_provider")
+
+    wallet_id = Column(String, ForeignKey('wallet.id'))
+    wallet = relationship("Wallet", back_populates="noid_provider")
+
     def gen(self):
         generated_value = f"{self.nam}_{self.dnam}_{self.sec_nam}"
         return generated_value
@@ -66,22 +93,42 @@ class NoidProvider(db.Model):
 class DecentralizedNameMappingAuthority(db.Model):
     __tablename__ = 'decentralized_name_mapping_authority'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(String(255))
     ror_id = Column(String(255))
     shoulder_prefix = Column(String(255))
     noid_provider_addr = Column(String(255))
     responsable = Column(String(255))
     section_authorities = Column(String(255))
 
+    noid_provider_id = Column(Integer, ForeignKey('noid_provider.id'))
+    noid_provider = relationship(
+        "NoidProvider", back_populates="decentralized_name_mapping_authority")
+
+    section_mapping_authorities = relationship(
+        "SectionMappingAuthority", back_populates="decentralized_name_mapping_authority")
+
+    wallet_responsable_id = Column(String, ForeignKey('wallet.id'))
+    wallet_responsable = relationship(
+        "Wallet", back_populates="decentralized_name_mapping_authority")
+
 
 class SectionMappingAuthority(db.Model):
     __tablename__ = 'section_mapping_authority'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(String(255))
     shoulder_prefix = Column(String(255))
     dNMA_id = Column(String(255))
     noid_provider_addr = Column(String(255))
     responsable = Column(String(255))
+
+    decentralized_name_mapping_authority_id = Column(
+        Integer, ForeignKey('decentralized_name_mapping_authority.id'))
+    decentralized_name_mapping_authority = relationship(
+        "DecentralizedNameMappingAuthority", back_populates="section_mapping_authorities")
+
+    wallet_responsable_id = Column(String, ForeignKey('wallet.id'))
+    wallet_responsable = relationship(
+        "Wallet", back_populates="section_mapping_authority")
 
 
 with app.app_context():
