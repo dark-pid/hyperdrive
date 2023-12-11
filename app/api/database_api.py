e291e1a484fa05c693d8b94b766d4a556095e63e
Francileudo Oliveira <sfrancileudo1@gmail.com>
2023-12-11 14:53:44 -0300

    feat: modification of the hyperdrive database structure

--- app/api/database_api.py
+++ app/api/database_api.py
@@ -6,9 +6,9 @@ database_api_blueprint = Blueprint(
     "database_api", __name__, url_prefix="/data")
 
 
-def add_user_to_database(naan, organization, email, wallet_private_key, ):
-    new_user = User(naan=naan, organization=organization,
-                    email=email, wallet_private_key=wallet_private_key)
+def add_user_to_database(email, password, wallet_private_key, ):
+    new_user = User(email=email, password=password,
+                    wallet_private_key=wallet_private_key)
     db.session.add(new_user)
     db.session.commit()
 
@@ -17,11 +17,10 @@ def add_user_to_database(naan, organization, email, wallet_private_key, ):
 def add_user():
     data = request.get_json()
 
-    naan = data.get('naan')
-    organization = data.get('organization')
     email = data.get('email')
+    password = data.get('password')
     wallet_private_key = data.get('wallet_private_key')
 
-    add_user_to_database(naan, organization, email, wallet_private_key)
+    add_user_to_database(email, password, wallet_private_key)
 
     return jsonify({'message': 'User added successfully'})
--- app/configure_database.py
+++ app/configure_database.py
@@ -24,14 +24,11 @@ class User(db.Model):
     __tablename__ = 'user_account'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    naan = Column(String(50))
-    organization = Column(db.String(50))
     email = Column(String(120), unique=True, nullable=False)
-    wallet_private_key = Column(String, ForeignKey('wallet.private_key'))
+    password = Column(String(250))
+    wallet_private_key = Column(String(250))
 
     transactions = relationship("Transaction", back_populates="user_account")
-
-    wallet = relationship("Wallet", back_populates="user_account")
 
 
 class Transaction(db.Model):
@@ -41,94 +38,10 @@ class Transaction(db.Model):
     tx_hash = Column(String(255))
     parameters = Column(JSON)
     status = Column(String(50))
-    parent_tx = Column(String(255))
+    parent_tx = Column(JSON)
 
     user_account_id = Column(Integer, ForeignKey('user_account.id'))
-    user_account = relationship("UserAccount", back_populates="transactions")
-
-
-class Wallet(db.Model):
-    __tablename__ = 'wallet'
-
-    private_key = Column(String(255), unique=True, primary_key=True)
-
-    noid_provider_id = Column(Integer, ForeignKey('noid_provider.id'))
-    noid_provider = relationship("NoidProvider", back_populates="wallet")
-
-    section_mapping_authority_id = Column(
-        String, ForeignKey('section_mapping_authority.id'))
-    section_mapping_authority = relationship(
-        "SectionMappingAuthority", back_populates="wallet_responsable")
-
-    decentralized_name_mapping_authority_id = Column(
-        String(255), ForeignKey('decentralized_name_mapping_authority.id'))
-    decentralized_name_mapping_authority = relationship(
-        "DecentralizedNameMappingAuthority", back_populates="wallet_responsable")
-
-
-class NoidProvider(db.Model):
-    __tablename__ = 'noid_provider'
-
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    nam = Column(String(255))
-    dnam = Column(String(255))
-    sec_nam = Column(String(255))
-    sep_token = Column(String(1))
-    noid_len = Column(Integer)
-
-    decentralized_name_mapping_authority_id = Column(String(255), ForeignKey(
-        'decentralized_name_mapping_authority.id'))
-    decentralized_name_mapping_authority = relationship(
-        "DecentralizedNameMappingAuthority", back_populates="noid_provider")
-
-    wallet_id = Column(String, ForeignKey('wallet.private_key'))
-    wallet = relationship("Wallet", back_populates="noid_provider")
-
-    def gen(self):
-        generated_value = f"{self.nam}_{self.dnam}_{self.sec_nam}"
-        return generated_value
-
-
-class DecentralizedNameMappingAuthority(db.Model):
-    __tablename__ = 'decentralized_name_mapping_authority'
-
-    id = Column(String(255), primary_key=True)
-    ror_id = Column(String(255))
-    shoulder_prefix = Column(String(255))
-    noid_provider_addr = Column(String(255))
-    responsable = Column(String(255))
-    section_authorities = Column(String(255))
-
-    noid_provider_id = Column(Integer, ForeignKey('noid_provider.id'))
-    noid_provider = relationship(
-        "NoidProvider", back_populates="decentralized_name_mapping_authority")
-
-    section_mapping_authorities = relationship(
-        "SectionMappingAuthority", back_populates="decentralized_name_mapping_authority")
-
-    wallet_responsable_id = Column(String, ForeignKey('wallet.private_key'))
-    wallet_responsable = relationship(
-        "Wallet", back_populates="decentralized_name_mapping_authority")
-
-
-class SectionMappingAuthority(db.Model):
-    __tablename__ = 'section_mapping_authority'
-
-    id = Column(String(255), primary_key=True)
-    ror_id = Column(String(255))
-    shoulder_prefix = Column(String(255))
-    dNMA_id = Column(String(255))
-    noid_provider_addr = Column(String(255))
-    responsable = Column(String(255))
-
-    decentralized_name_mapping_authority_id = Column(
-        String(255), ForeignKey('decentralized_name_mapping_authority.id'))
-    decentralized_name_mapping_authority = relationship(
-        "DecentralizedNameMappingAuthority", back_populates="section_mapping_authorities")
-
-    wallet_responsable_id = Column(String, ForeignKey('wallet.private_key'))
-    wallet_responsable = relationship(
-        "Wallet", back_populates="section_mapping_authority")
+    user_account = relationship("User", back_populates="transactions")
 
 
 with app.app_context():
