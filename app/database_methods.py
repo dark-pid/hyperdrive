f03729fe30948e6e059d6ea8cb520d7bbd568907
Francileudo Oliveira <sfrancileudo1@gmail.com>
2024-01-04 15:13:48 -0300

    feat: adding environment variable for jwt token expiration time

--- app/database_methods.py
+++ app/database_methods.py
@@ -6,6 +6,10 @@ from flask_jwt_extended import create_access_token
 
 from datetime import timedelta
 
+from util.config_manager import ConfigManager
+
+config_manager = ConfigManager()
+
 app = create_app()
 
 
@@ -16,13 +20,19 @@ class UserIsNone(Exception):
 def authenticate(email, password):
     with app.app_context():
 
+        try:
+            AUTH_TOKEN_EXPIRATION_TIME = int(
+                config_manager.get_auth_token_expiration_time())
+        except (ValueError, TypeError):
+            AUTH_TOKEN_EXPIRATION_TIME = 6
+
         user = User.query.filter_by(email=email).first()
 
         access_token = None
 
         if user != None and check_password_hash(user.password, password):
             access_token = create_access_token(
-                identity=user.id, expires_delta=timedelta(hours=6))
+                identity=user.id, expires_delta=timedelta(hours=AUTH_TOKEN_EXPIRATION_TIME))
 
             return access_token
         else:
