0ad64ccceb417566fe4267aa5bcc8f6bd92615cb
Thiago <thiagonobrega@computacao.ufcg.eu.br>
2025-03-29 11:42:40 -0300

    Refactor update endpoint to improve error handling and processing logic; add test cases for various UPDATE scenarios

--- app/services/load_api.py
+++ app/services/load_api.py
@@ -155,13 +155,11 @@ def check_json():
 
 
 @load_api.route('/update', methods=['post'])
-def check_json():
+def update_metadata():
     data = request.json
     items = data.get('items', [])
     erros = []
-    dnam_pk = ''
-    load_time = 0
-    r_time = 0
+    update_time = 0
 
     try:
         account, dnam_wallet, dark_gw, dark_map = check_account(data)
@@ -172,70 +170,90 @@ def check_json():
         erros.append("Invalid private key")
     except ValueError:
         erros.append("Invalid private key")
-
-    # recuperar o pid
-    try:
-        dark_id = data.get('dark_id')
-        if not dark_id:
-            erros.append('No dark_id provided')
-    except ValueError as e:
-        erros.append('No dark_id provided')
-    try:
-        url = data.get('url')
-        if not dark_id:
-            erros.append('No url provided')
-    except ValueError as e:
-        erros.append('No url provided')
-        
-
-    try:
-        dark_pid = dark_map.get_pid_by_ark(dark_id)
-    except Exception as e:
-        erros.append('dark_id not founded')
-        # resp = jsonify({'status' : 'Unable to recovery (' + str(dark_id) + ')', 'block_chain_error' : str(e)},)
-        # resp_code = 500 # colocar um erro especifico?
-
+    
+    # caso tenha parametros invalidos
     if len(erros) > 0:
         resp = {'erros' :  erros , 'params' : str(data)}
         return jsonify(resp), 500
-
-    item_data = {'dark_id': dark_id , 'old_url': dark_pid.external_url, 'update_url' : url}
-    if is_valid_url(url):
-        try:
-            ark_hash_obj = dark_pid.pid_hash
-            ark_hash_hex_val = ark_hash_obj.hex()
-            item_data['ark_hash'] = ark_hash_hex_val
-            tx = dark_map.async_set_url(ark_hash_obj,url)
-            txr = tx.hex()
-            item_data['tx_recipt'] = txr
-        except Exception as e: 
-            item_data['error'] = 'bockchain error'
-            item_data['error_desc'] = str(e)
-    else:
-        item_data['error'] = 'invalid url'
-
-
     
-    # recuperando os arks
-    # start_time = time.time()
-    # saida = []
-    # for park in processados:
-    #     ark_hash = park['ark_hash']
-    #     ark = dark_map.get_pid_by_hash(ark_hash).to_dict()
-    #     park['ark'] = ark['ark']
-    #     park['ark_url'] = ark['external_url']
+    processados = []
+    nao_processados = []
+    start_time = time.time()
+    for item in items:
+
+        # recuperar o pid
+        parametros_validos = True
+        try:
+            dark_id = item.get('dark_id')
+            if not dark_id:
+                item['error'] = 'No valid dark_id provided'
+                nao_processados.append(item)
+                parametros_validos = False
+        except ValueError as e:
+            erros.append('No dark_id provided')
+            item['error'] = 'No valid dark_id provided'
+            nao_processados.append(item)
+            parametros_validos = False
+        try:
+            update_url = item.get('url')
+            item['update_url'] = update_url
+            del item['url']
+
+            if not update_url:
+                item['error'] = 'No url provided'
+                nao_processados.append(item)
+                parametros_validos = False
+        except ValueError as e:
+            item['error'] = 'No url provided'
+            nao_processados.append(item)
+            parametros_validos = False
         
-    #     saida.append(park)
-    # end_time = time.time()
-    # r_time = end_time - start_time
+        if parametros_validos:
+            try:
+                dark_pid = dark_map.get_pid_by_ark(dark_id)
+            except Exception as e:
+                item['error'] = 'dark_id not found'
+                nao_processados.append(item)
+                break
+                # resp = jsonify({'status' : 'Unable to recovery (' + str(dark_id) + ')', 'block_chain_error' : str(e)},)
+                # resp_code = 500 # colocar um erro especifico?
+
+            item['update_url'] = update_url
+
+            if is_valid_url(update_url):
+                try:
+                    ark_hash_obj = dark_pid.pid_hash
+                    ark_hash_hex_val = ark_hash_obj.hex()
+                    item['ark_hash'] = ark_hash_hex_val
+                    tx = dark_map.async_set_url(ark_hash_obj,update_url)
+                    txr = tx.hex()
+                    item['tx_recipt'] = txr
+                    item['previous_url'] = dark_pid.external_url
+                    processados.append(item)
+                except Exception as e: 
+                    item['error'] = 'bockchain error'
+                    item['error_desc'] = str(e)
+                    nao_processados.append(item)
+            else:
+                item['error'] = 'invalid url'
+                nao_processados.append(item)
+                print("aqui")
+
         
+    end_time = time.time()
+    update_time = end_time - start_time
 
     response = {
             "wallet_addr": account.address ,
             "action": 'update',
-            "data" : item_data,
-            "timestamp": datetime.now().timestamp()
+            "update_time" : update_time,
+            "timestamp": datetime.now().timestamp(),
+            "update_pids": processados
     }
+
+    if len(nao_processados) != 0:
+        response["not_updated_pids"] = nao_processados
+        
 
     # 
     os.makedirs('logs', exist_ok=True)
